e22ad60cb56ca3ddceb68d05414bb6390c76a031
VTSingle <madheader92@gmail.com>
2019-02-25 10:55:16 +0200

    Update index.js

--- app/constants/tabIcon/index.js
+++ app/constants/tabIcon/index.js
@@ -1,10 +1,11 @@
 import React, {} from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {green, black} from "../colors";
 
 export const iconList =  ({ icon, focused }) => (
-    focused ? <Icon name="list-ul" size={30} color="#004d00"/> : <Icon name="list-ul" size={30} color="#000000"/>
+    focused ? <Icon name="list-ul" size={30} color={green}/> : <Icon name="list-ul" size={30} color={black}/>
 );
 
 export const iconAdd =  ({ icon, focused }) => (
-    focused ? <Icon name="plus-square" size={30} color="#004d00"/> : <Icon name="plus-square" size={30} color="#000000"/>
+    focused ? <Icon name="plus-square" size={30} color={green}/> : <Icon name="plus-square" size={30} color={black}/>
 );
